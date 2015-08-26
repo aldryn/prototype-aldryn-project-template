f7be941c67978ee0911f512f2f69e76f1d9cee70
Stefan Foulis <stefan@foulis.ch>
2015-08-26 17:38:43 +0200

    add last url setting

--- addons-dev/aldryn-addons/aldryn_addons/urls.py
+++ addons-dev/aldryn-addons/aldryn_addons/urls.py
@@ -20,14 +20,26 @@ from django.conf import urls
 
 
 def patterns():
-    return [
+    pats = [
         urls.url(r'^', urls.include(url))
         for url in getattr(settings, 'ADDON_URLS', [])
     ]
+    last_url = getattr(settings, 'ADDON_URLS_LAST', None)
+    if last_url:
+        pats.append(
+            urls.url(r'^', urls.include(last_url))
+        )
+    return pats
 
 
 def i18n_patterns():
-    return [
+    pats = [
         urls.url(r'^', urls.include(url))
         for url in getattr(settings, 'ADDON_URLS_I18N', [])
     ]
+    last_url = getattr(settings, 'ADDON_URLS_I18N_LAST', None)
+    if last_url:
+        pats.append(
+            urls.url(r'^', urls.include(last_url))
+        )
+    return pats
