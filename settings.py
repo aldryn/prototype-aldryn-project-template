1c398c64368409328986cc4625e3ea15c28d637f
Stefan Foulis <stefan@foulis.ch>
2015-09-03 17:18:47 +0200

    cleanup

--- settings.py
+++ settings.py
@@ -4,14 +4,14 @@ INSTALLED_ADDONS = [
     # <INSTALLED_ADDONS>  # Warning: this is auto-generated. Manual changes will be overwritten.
     'aldryn-django',
     'aldryn-cms',
-    'aldryn-newsblog',
     # </INSTALLED_ADDONS>
 ]
-
 
 import aldryn_addons.settings
 aldryn_addons.settings.load(locals())
 
+
+# all django settings can be altered here
 
 INSTALLED_APPS.extend([
     # add you project specific apps here
@@ -24,3 +24,4 @@ TEMPLATE_CONTEXT_PROCESSORS.extend([
 MIDDLEWARE_CLASSES.extend([
     # add your own middlewares here
 ])
+
